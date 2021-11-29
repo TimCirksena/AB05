3f2c0c4fb7bdcff0c6138f2ff918ec7caa3137e0
tim.cirksena <tim.cirksena@outlook.de>
2021-11-29 11:28:12 +0100

    sack

--- app/src/main/java/com/example/ab05/MainActivity.java
+++ app/src/main/java/com/example/ab05/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
 import android.os.Bundle;
 import android.widget.ImageView;
@@ -19,12 +20,25 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
         this.bitmap = Bitmap.createBitmap(this.breite, this.hoehe, Bitmap.Config.ARGB_8888);
         this.canvas = new Canvas(this.bitmap);
         this.imageView = new ImageView(this);
         this.imageView.setImageBitmap(this.bitmap);
         this.paint = new Paint();
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(imageView);
+        this.canvas.drawColor(Color.argb(255, 0, 0, 255));
+        this.paint.setTextSize(50);
+        this.halloWelt();
+
+
+    }
+
+    private void halloWelt() {
+        String text = "Hallo Welt!";
+        float textWidth = this.paint.measureText(text);
+        this.paint.setColor(Color.WHITE);
+        this.canvas.drawText(text, breite / 2 - textWidth / 2, 100, this.paint);
     }
 }

df7d7654b7c9d6420e7461a4ad2f1bab8d265ee4
tim.cirksena <tim.cirksena@outlook.de>
2021-11-30 14:13:06 +0100

    sack

--- app/src/main/java/com/example/ab05/MainActivity.java
+++ app/src/main/java/com/example/ab05/MainActivity.java
@@ -17,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
     private Paint paint;
     private int hoehe = 800;
     private int breite = 800;
+    private int textsize = 50;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,10 +30,14 @@ public class MainActivity extends AppCompatActivity {
         this.paint = new Paint();
         setContentView(imageView);
         this.canvas.drawColor(Color.argb(255, 0, 0, 255));
-        this.paint.setTextSize(50);
+        this.paint.setTextSize(textsize);
         this.halloWelt();
+        this.halloNachbarn();
+        //this.zeichneSmiley(100);
 
-
+    }
+    private void halloNachbarn(){
+        textZentrieren("Hallo Maxi und Tom!", 150);
     }
 
     private void halloWelt() {
@@ -40,5 +45,15 @@ public class MainActivity extends AppCompatActivity {
         float textWidth = this.paint.measureText(text);
         this.paint.setColor(Color.WHITE);
         this.canvas.drawText(text, breite / 2 - textWidth / 2, 100, this.paint);
+    }
+    private void textZentrieren(String text, int y){
+        float textWidth = this.paint.measureText(text);
+        this.canvas.drawText(text, breite / 2 - textWidth / 2, y, this.paint);
+    }
+    public void zeichneSmiley(int radius){
+        this.paint.setColor(Color.GREEN);
+        this.paint.setStrokeWidth(radius);
+        this.paint.setStyle(Paint.Style.STROKE);
+        this.canvas.drawCircle(hoehe/2, breite/2, radius,this.paint);
     }
 }
